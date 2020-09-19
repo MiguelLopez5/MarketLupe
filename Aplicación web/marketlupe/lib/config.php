f2a23f4f301d18c6a795abd2e1fa8fc135aaf9c6
MiguelLopez <mlopez340@misena.edu.co>
2020-09-19 16:20:51 -0500

    Creada la rama para documentacion de la aplicacion

--- Aplicación web/marketlupe/lib/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-  define("HOST", "localhost");
-  define("DATABASE", "sordmark");
-  define("CHARSET", "utf8");
-  define("USER", "root");
-  define("PASSWORD", "");
-?>
--- Aplicación web/marketlupe/views/layouts/footer.php
+++
@@ -1,7 +0,0 @@
-
-<footer>
-  <h3>Super Guadalupe</h3>
-  MarketLupe ©2019 Todos los derechos reservados | powered by <a class="pag" href="#">Softdevelopers</a>
-</footer>
-</body>
-</html>
